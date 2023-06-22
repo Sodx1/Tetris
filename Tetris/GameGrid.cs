02fe32bb2bbeaf2b0f652642190603b8536528ee
DESKTOP-TKSLLHO\Sodx <anthonyprorok@hotmail.com>
2023-06-22 14:27:14 +0700

    Add project files.

---
+++ Tetris/GameGrid.cs
@@ -0,0 +1,25 @@
+﻿namespace Tetris
+{
+    //Grid Tetris
+    public class GameGrid
+    {
+        private readonly int[,] grid;
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int r, int c]
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
+
+        //Constructor Game Grid
+        public GameGrid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[rows, columns];
+        }
+
+    }
+}
