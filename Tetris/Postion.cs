3aff4ef874be71b91630ab8851a75981abccbe29
DESKTOP-TKSLLHO\Sodx <anthonyprorok@hotmail.com>
2023-06-23 15:43:53 +0700

    Work Postion and  add Abstract class Block

--- Tetris/Postion.cs
+++ Tetris/Postion.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tetris
+﻿namespace Tetris
 {
-    internal class Postion
+    public class Postion
     {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Postion(int row, int column)        {
+            Row = row;
+            Column = column;
+        }
+
     }
 }
