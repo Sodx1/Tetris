b8590e42b271312babc1d832d593d94c54fd5797
DESKTOP-TKSLLHO\Sodx <anthonyprorok@hotmail.com>
2023-06-29 15:10:32 +0700

    work GameState and BlockQueue

---
+++ Tetris/BlockQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        //Масив доступных тетраминов
+        private readonly Block[] _blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock(),
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+        //Очередь блоков
+        public BlockQueue() 
+        {
+            NextBlock = RandomBlock();
+        }
+        //Задаем рандомость блоков
+        public Block RandomBlock()
+        {
+            return _blocks[random.Next(_blocks.Length)];    
+        }
+
+        //Обработка блоков чтобы они дважды не повторялись
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            } 
+            while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
